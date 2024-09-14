580d6f4202ec53342e4171e970b36bc6cdb36b1d
Ryan Chang <chang729@purdue.edu>
2024-09-13 22:54:40 -0400

    test2

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB connecte'))
+.then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
 // Routes
